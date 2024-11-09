5a61ba131347146d5d39c12545e28d60bf4d287b
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-11-09 14:47:31 -0700

    REASON: The Twilight Forest Unofficial is gone

--- fabric/src/main/java/net/mehvahdjukaar/every_compat/fabric/EveryCompatFabric.java
+++ fabric/src/main/java/net/mehvahdjukaar/every_compat/fabric/EveryCompatFabric.java
@@ -67,10 +67,10 @@ public class EveryCompatFabric extends EveryCompat implements ModInitializer {
         addModule("variantvanillablocks", () -> VariantVanillaBlocksModule::new);
         addModule("wilderwild", () -> WilderWildModule::new);
         addModule("woodenhoppers", () -> WoodenHoppersModule::new);
-        addModule("twilightforest", () -> TwilightForestModule::new); // Support The-Twilight-Forest-Unofficial
+//        addModule("twilightforest", () -> TwilightForestModule::new); //!! NOT AVAILABLE
 
 // ============================================== DISABLED FOR A REASON ============================================= \\
-//        addModule("architects_palette", () -> ArchitectsPaletteModule::new); // Not available
+//        addModule("architects_palette", () -> ArchitectsPaletteModule::new); //!! NOT AVAILABLE
 
 // ===================================================== OTHERS ===================================================== \\
         if (EveryCompat.OLD_FD) EveryCompatAPI.registerModule(new FarmersDelightModuleOld("farmersdelight"));
