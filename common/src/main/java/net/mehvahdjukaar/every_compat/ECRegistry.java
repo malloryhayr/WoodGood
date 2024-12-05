0673653fab4c6a4202e396032cc341f2f090fc74
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-12-04 22:45:51 -0700

    ECRegistry: Re-organized

--- common/src/main/java/net/mehvahdjukaar/every_compat/ECRegistry.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/ECRegistry.java
@@ -16,16 +16,15 @@ public class ECRegistry {
 
     }
 
-
-    public static final Supplier<AllWoodItem> ALL_WOODS = RegHelper.registerItem(EveryCompat. res("all_woods"), AllWoodItem::new);
+    public static final Supplier<AllWoodItem> ALL_WOODS = RegHelper.registerItem(EveryCompat.res("all_woods"), AllWoodItem::new);
 
     @Nullable
-    public static final RegSupplier<CreativeModeTab> MOD_TAB =
-            ECConfigs.TAB_ENABLED.get() ?
+    public static final RegSupplier<CreativeModeTab> MOD_TAB = ECConfigs.TAB_ENABLED.get() ?
                     RegHelper.registerCreativeModeTab(EveryCompat.res(EveryCompat.MOD_ID),
                             true,
                             builder -> builder.icon(() -> ALL_WOODS.get().getDefaultInstance())
                                     .backgroundSuffix("item_search.png")
                                     .title(Component.translatable("itemGroup.everycomp.everycomp"))
-                                    .build()) : null;
+                                    .build())
+                    : null;
 }
