ac791d521dca39f03ea356c4ed9e84b355c78a56
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-12-07 11:48:40 +0100

    only in

--- common/src/main/java/net/mehvahdjukaar/every_compat/misc/AllWoodItem.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/misc/AllWoodItem.java
@@ -1,6 +1,8 @@
 package net.mehvahdjukaar.every_compat.misc;
 
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.mehvahdjukaar.moonlight.api.client.ICustomItemRendererProvider;
 import net.mehvahdjukaar.moonlight.api.client.ItemStackRenderer;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
@@ -14,6 +16,7 @@ public class AllWoodItem extends Item implements ICustomItemRendererProvider {
         super(new Properties());
     }
 
+    @Environment(EnvType.CLIENT)
     @Override
     public Supplier<ItemStackRenderer> getRendererFactory() {
         return () -> new BlockTypeCycleItemRenderer<>(WoodType.class);
