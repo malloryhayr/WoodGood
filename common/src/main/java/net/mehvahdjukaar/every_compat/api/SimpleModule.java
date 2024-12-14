d0ce58f05a25999d82eeb34e22473c778fde767a
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-12-14 11:59:03 +0100

    moved hardcoded checks

--- common/src/main/java/net/mehvahdjukaar/every_compat/api/SimpleModule.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/api/SimpleModule.java
@@ -3,12 +3,15 @@ package net.mehvahdjukaar.every_compat.api;
 import net.mehvahdjukaar.every_compat.EveryCompat;
 import net.mehvahdjukaar.every_compat.dynamicpack.ClientDynamicResourcesHandler;
 import net.mehvahdjukaar.every_compat.dynamicpack.ServerDynamicResourcesHandler;
+import net.mehvahdjukaar.every_compat.misc.HardcodedStuff;
 import net.mehvahdjukaar.moonlight.api.events.AfterLanguageLoadEvent;
 import net.mehvahdjukaar.moonlight.api.misc.Registrator;
 import net.mehvahdjukaar.moonlight.api.platform.ClientHelper;
 import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
 import net.mehvahdjukaar.moonlight.api.platform.RegHelper;
 import net.mehvahdjukaar.moonlight.api.set.BlockType;
+import net.mehvahdjukaar.moonlight.api.set.leaves.LeavesType;
+import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
@@ -184,7 +187,6 @@ public class SimpleModule extends CompatModule {
     }
 
 
-    // ugly mess. Too coupled with wood types and too many hardcoded exceptions
     //TODO: improve
     public boolean isEntryAlreadyRegistered(String name, BlockType woodType, Registry<?> registry) {
         //ec:twigs/bop/willow_table
@@ -192,79 +194,24 @@ public class SimpleModule extends CompatModule {
 
         String woodFrom = woodType.getNamespace();
 
-        String n1 = woodFrom + "/" + name; //quark/blossom_chair
-        String n2 = woodFrom + "_" + name; //quark_blossom_chair
+        String slashConvention = woodFrom + "/" + name; //quark/blossom_chair
+        String understoreConvention = woodFrom + "_" + name; //quark_blossom_chair
 
         if (this.getAlreadySupportedMods().contains(woodFrom)) return true;
 
-        // Better Nether & Better End have stripped_bark as stripped_wood but bark from Bewitchment caused EC to skip
-        if (woodFrom.matches("betternether|betterend") && shortenedId().equals("bw")) return false;
-
-        // Discarding Dynamic Trees and its addons
-        if (woodFrom.contains("dynamictrees") || woodFrom.contains("dt")) return true;
-
-        // Garden-Of-The-dead's whistle must be skipped for branches from Regions-Unexplored
-        // Nether's Exoticism & Snifferent already has branches, branches from Regions-Unexplored is not needed
-        if ((woodFrom.matches("gardens_of_the_dead|snifferent") ||
-                woodType.getId().toString().equals("nethers_exoticism:jabuticaba")) && name.contains("branch"))
-            return true;
-
-        // Quark & Woodworks have chest & trapped_chest
-        if (woodFrom.equals("quark") && shortenedId().equals("abnww") && name.contains("chest")) return true;
-
-        // Macaw's Fences&Walls or MrCrayFish's Furniture - hedges will be skipped because Quark already has hedges
-        if (woodFrom.equals("quark") && (shortenedId().equals("mcf") || shortenedId().equals("cfm"))) return false;
-
-        // Create's windows will be skipped blc [Let's do] Blooming Nature & Meadow already has windows
-        if ((woodFrom.equals("bloomingnature") || woodFrom.equals("meadow")) && name.contains("window")) return false;
-
-        // ArchitectPalette's boards will be skipped blc Upgrade-Aqautic already has boards but have no recipes &
-        // no item in CreativeMode
-        if (woodFrom.equals("upgrade_aquatic") && (name.equals("driftwood_boards") || name.equals("river_boards")))
-            return false;
-
-        // Similar to above, Architect's Palette - boards will be skipped due to the existing boards in Autumnity
-        if (woodFrom.equals("autumnity") && name.equals("maple_boards")) return false;
-
-        // check if TerraFirmaCraft (tfc) mod exist, then won't discards wood types
-        if (woodFrom.equals("tfc")) return false;
+        if (woodType instanceof WoodType wt) {
+            Boolean hardcoded = HardcodedStuff.isWoodBlockAlreadyRegistered(name, wt, modId, shortenedId());
+            if (hardcoded != null) return hardcoded;
+        } else if (woodType instanceof LeavesType lt) {
+            Boolean hardcoded = HardcodedStuff.isLeavesBlockAlreadyRegistered(name, lt, modId, shortenedId());
+            if (hardcoded != null) return hardcoded;
+        }
 
         //discards wood types from this mod
         if (woodFrom.equals(modId)) return true; //quark, blossom
 
-        if (woodType.getId().toString().equals("ecologics:azalea")) {
-            if (modId.equals("quark")) return false; //ecologics and quark azalea. tbh not sure why needed
-        }
-        if (woodType.getId().toString().equals("twilightforest:mangrove")) {
-            return name.equals("mangrove_chest");//mangrove waaa so much pain
-        }
-
-        if (woodType.getId().toString().equals("architects_palette:twisted")) {
-            return name.equals("vct:twisted_crafting_table");
-        }
-        if (woodType.getId().toString().equals("biomesoplenty:fir")) {
-            return name.equals("vct:fir_crafting_table");
-        }
-        if (woodType.getId().toString().equals("biomesoplenty:jacaranda")) {
-            return name.equals("vct:jacaranda_crafting_table");
-        }
-        if (woodType.getId().toString().equals("biomesoplenty:maple")) {
-            return name.equals("vct:maple_crafting_table");
-        }
-        if (woodType.getId().toString().equals("ecologics:azalea")) {
-            return name.equals("vct:azalea_crafting_table");
-        }
-        if (woodType.getId().toString().equals("ecologics:walnut")) {
-            return name.equals("vct:walnut_crafting_table");
-        }
-
         if (registry.containsKey(new ResourceLocation(modId, name)) || //ones from the mod they are from. usually include vanilla types
-                registry.containsKey(new ResourceLocation(modId, n2))) return true;
-        if (this.shortenedId().equals("af")) return false; //hardcoding
-        // if (this.shortenedId().equals("ap")) return false; //hardcoding dont remember why i had this. Incase you want o
-        if (this.shortenedId().equals("vs")) return false; //we always register everything for these
-        if (this.shortenedId().equals("abnww") && woodFrom.equals("architects_palette"))
-            return false; //we always register everything for these
+                registry.containsKey(new ResourceLocation(modId, understoreConvention))) return true;
 
         if (registry.containsKey(new ResourceLocation(woodFrom, name))) return true;
 
@@ -273,11 +220,12 @@ public class SimpleModule extends CompatModule {
             //if the wood is from the mod this adds compat for && it supports this block type
             if (c.woodsFrom().contains(woodFrom) && c.blocksFrom().contains(modId)) {
                 if (registry.containsKey(new ResourceLocation(compatModId, name))) return true;
-                if (registry.containsKey(new ResourceLocation(compatModId, n1))) return true;
-                if (registry.containsKey(new ResourceLocation(compatModId, n2))) return true;
+                if (registry.containsKey(new ResourceLocation(compatModId, slashConvention))) return true;
+                if (registry.containsKey(new ResourceLocation(compatModId, understoreConvention))) return true;
             }
         }
         return false;
     }
+
 
 }
--- common/src/main/java/net/mehvahdjukaar/every_compat/misc/BlockTypeCycleItemRenderer.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/misc/BlockTypeCycleItemRenderer.java
@@ -2,6 +2,7 @@ package net.mehvahdjukaar.every_compat.misc;
 
 import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.mehvahdjukaar.moonlight.api.client.ItemStackRenderer;
 import net.mehvahdjukaar.moonlight.api.set.BlockSetAPI;
@@ -20,7 +21,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-@Environment(net.fabricmc.api.EnvType.CLIENT)
+@Environment(EnvType.CLIENT)
 public abstract class BlockTypeCycleItemRenderer<T extends BlockType> extends ItemStackRenderer {
 
     private final List<String> childKeys = new ArrayList<>();
