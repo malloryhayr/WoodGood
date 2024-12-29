ddbef01d399d9efe3739b55490ef02a22a8bfcfa
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-12-28 18:03:28 -0700

    build.gradle(FABRIC|FORGE): Updated Regions Unexplored to v0.5.6

--- fabric/src/main/java/net/mehvahdjukaar/every_compat/modules/fabric/regions_unexplored/RegionsUnexploredModule.java
+++ fabric/src/main/java/net/mehvahdjukaar/every_compat/modules/fabric/regions_unexplored/RegionsUnexploredModule.java
@@ -38,7 +38,7 @@ import java.util.Map;
 
 import static net.mehvahdjukaar.every_compat.common_classes.TagUtility.createAndAddCustomTags;
 
-// SUPPORT: v0.5.5+
+// SUPPORT: v0.5.6+
 public class RegionsUnexploredModule extends SimpleModule {
     public final SimpleEntrySet<WoodType, Block> branchs;
     public final SimpleEntrySet<LeavesType, Block> shrubs;
